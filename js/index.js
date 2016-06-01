576f6ba43ccafc17142b8cf80375b37ebf3ad400
Anna Bezrukova <annelia_55@mail.ru>
2016-06-01 23:12:49 +0300

    scrollspy js

--- js/index.js
+++ js/index.js
@@ -1,11 +1,3 @@
-/*
-$(function() {
-    $('body').on('click', '.page-scroll a', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
+$('[data-spy="scroll"]').each(function () {
+    var $spy = $(this).scrollspy('refresh')
 });
-*/
